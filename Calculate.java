661fd59624b509f9269553173613ad47825f6823
Tristan Lonsway <tblonswa@syr.edu>
2019-03-21 22:55:13 -0400

    Significantly increased speed of projection matrix, now almost 10mil/sec on i5 4590s

--- Calculate.java
+++ Calculate.java
@@ -1,18 +1,32 @@
 public class Calculate {
-    private static double[] vecmatmult(float[] v, float[][] m) {
-        double[] r = new double[]{(m[0][0]*v[0])+(m[0][1]*v[1])+(m[0][2]*v[2]),(m[1][0]*v[0])+(m[1][1]*v[1])+(m[1][2]*v[2]),(m[2][0]*v[0])+(m[2][1]*v[1])+(m[2][2]*v[2])};
+    public static double[] vecmatmult(float[] v, float[][] m) {
+        double[] r = new double[]{(m[0][0]*v[0])+(m[0][1]*v[1])+(m[0][2]*v[2])+(m[0][3]*v[3]),(m[1][0]*v[0])+(m[1][1]*v[1])+(m[1][2]*v[2])+(m[1][3]*v[3]),(m[2][0]*v[0])+(m[2][1]*v[1])+(m[2][2]*v[2])+(m[2][3]*v[3]),(m[3][0]*v[0])+(m[3][1]*v[1])+(m[3][2]*v[2])+(m[3][3]*v[3])};
         return r;
+    }
+    public static double[] slowvecmath(float[] v, float[][] m) {
+        double tempadd = 0;
+        double[] result = new double[4];
+        for (int r1=0; r1<v.length; r1++) {
+            for (int c1=0; c1<v.length; c1++) {
+                tempadd += m[r1][c1]*v[c1];
+            }
+            result[r1]=tempadd;
+            tempadd = 0;
+        }
+        return result;
     }
     public static double[] translate(float[] vector, double x, double y, double z) {
         float[][] matrix = new float[][]{{1,0,0,(float)x},
-                                           {0,1,0,(float)y},
-                                           {0,0,1,(float)z}};
+                                         {0,1,0,(float)y},
+                                         {0,0,1,(float)z},
+                                         {0,0,0,1}};
         return vecmatmult(vector,matrix);   
     }
     public static double[] scale(float[] vector, double x, double y, double z) {
         float[][] matrix = new float[][]{{(float)x,0,0,0},
-                                           {0,(float)y,0,0},
-                                           {0,0,(float)z,0}};
+                                         {0,(float)y,0,0},
+                                         {0,0,(float)z,0},
+                                         {0,0,0,1}};
         return vecmatmult(vector,matrix);  
     }
     public static double[] rotate(float[] vector, char c, double angle) {
@@ -22,18 +36,21 @@ public class Calculate {
         if (c=='x') {
             matrix = new float[][]{{1,0,0,0},
                                    {0,acos,-1*asin,0},
-                                   {0,asin,acos,0}};     
+                                   {0,asin,acos,0},
+                                   {0,0,0,0}};
         }
         if (c=='y') {
             matrix = new float[][]{{acos,0,asin,0},
                                    {0,1,0,0},
-                                   {-1*asin,0,acos,0}};              
+                                   {-1*asin,0,acos,0},
+                                   {0,0,0,0}};
             
         }
         if (c=='z') {
             matrix = new float[][]{{acos,-1*asin,0,0},
                                    {asin,acos,0,0},
-                                   {0,0,1,0}};              
+                                   {0,0,1,0},
+                                   {0,0,0,0}};
             
         }
         return vecmatmult(vector,matrix);  
@@ -41,10 +58,14 @@ public class Calculate {
     public static double[] project2D(float[] vector, double fov, double aspect, double near, double far) {
         float fn=(float)(far-near);
         float mathtf=(float)(Math.atan(fov/2));
-        float[][] matrix = new float[][]{   {mathtf,0,0,0},
-                                              {0,mathtf,0,0},
-                                              {0,0,-1*(float)((far+near)/fn),-1*(float)((2*(far*near))/fn)}};        
-        double[] result = vecmatmult(vector,matrix);
+        //float[][] matrix = new float[][]{   {mathtf,0,0,0},
+        //                                    {0,mathtf,0,0},
+        //                                    {0,0,-1*(float)((far+near)/fn),-1*(float)((2*(far*near))/fn)},
+        //                                    {0,0,0,0}};
+        //double[] result = vecmatmult(vector,matrix);
+        //double[] r = new double[]{(mathtf*v[0])+(0*v[1])+(0*v[2]),(m[1][0]*v[0])+(m[1][1]*v[1])+(m[1][2]*v[2]),(m[2][0]*v[0])+(m[2][1]*v[1])+(m[2][2]*v[2])};
+        double[] result = new double[]{(mathtf*vector[0]),(mathtf*vector[1])};
+        //VECTOR MULTIPLICATION NOT FINISHED, FOURTH COLUMN IN MATRIX NEEDED
         double x = -1*(result[0]);
         double y = -1*(result[1]);
         x+=1;
@@ -56,9 +77,11 @@ public class Calculate {
     public static void vecmatmulttest(int runs) {
         float[][] matrix = new float[][]{{(float)Math.random(),(float)Math.random(),(float)Math.random(),(float)Math.random()},
                                                {(float)Math.random(),(float)Math.random(),(float)Math.random(),(float)Math.random()},
+                                               {(float)Math.random(),(float)Math.random(),(float)Math.random(),(float)Math.random()},
                                                {(float)Math.random(),(float)Math.random(),(float)Math.random(),(float)Math.random()}};
-        float[] vector = new float[]{(float)Math.random(), (float)Math.random(), (float)Math.random()};
+        float[] vector = new float[]{(float)Math.random(), (float)Math.random(), (float)Math.random(), (float)Math.random()};
         long fastsum=0;
+        long slowsum=0;
         long projsum=0;
         for(int i=0;i<runs;i++) {
             long startf = System.currentTimeMillis();
@@ -67,6 +90,12 @@ public class Calculate {
                 vecmatmult(vector,matrix);
                 calcf++;
             }
+            long starts = System.currentTimeMillis();
+            int calcs=0;
+            while(System.currentTimeMillis()-1000<starts) {
+                slowvecmath(vector,matrix);
+                calcs++;
+            }
             long startp = System.currentTimeMillis();
             int calcp=0;
             while(System.currentTimeMillis()-1000<startp) {
@@ -74,10 +103,15 @@ public class Calculate {
                 calcp++;
             }
             fastsum+=calcf;
+            slowsum+=calcs;
             projsum+=calcp;
             System.out.println("fastmath calculations per second: " + calcf);
+            System.out.println("slowmath calculations per second: " + calcs);
             System.out.println("projections per second: " + calcp);
         }
-        System.out.println("\n\nAverage fastmath cps: " + fastsum/runs + "\nAverage projection cps: " + projsum/runs);
+        System.out.println("\n\nAverage fastmath cps: " + fastsum/runs + "\nAverage slowmath cps: " + slowsum/runs + "\nAverage projection cps: " + projsum/runs);
+    }
+    public static void main(String[] args) {
+        vecmatmulttest(Integer.parseInt(args[0]));
     }
 }
